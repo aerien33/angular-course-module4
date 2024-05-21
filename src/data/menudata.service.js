e32a9ce42a3cd07d673296eb42ebb3a44c8b379e
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-05-21 12:31:36 +0200

    Implement getItemsForCategory() in MenuDataService

--- src/data/menudata.service.js
+++ src/data/menudata.service.js
@@ -4,18 +4,27 @@
 angular.module('data')
 .service('MenuDataService', MenuDataService);
 
-MenuDataService.$inject = ['$http']
+MenuDataService.$inject = ['$http'];
 function MenuDataService($http) {
-  var service = this;
+    var service = this;
 
-  service.getAllCategories = function () {
-    return $http({
+    service.getAllCategories = function () {
+        return $http({
                method: "GET",
                url: "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json"
-           }).then(function (result) {
-               return result.data;
-           });
-  }
+        }).then(function (result) {
+            return result.data;
+        });
+    }
+
+    service.getItemsForCategory = function (categoryShortName) {
+        return $http({
+                method: "GET",
+                url: ("https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/" + categoryShortName + ".json")
+        }).then(function (result) {
+            return result.data.menu_items;
+        });
+    }
 
 }
 
