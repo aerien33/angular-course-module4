63ed16a20fc8bd56a39479f0251faa0f318b1a6a
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-05-23 16:19:41 +0200

    Set categories.menuItems state for MenuApp module using ui-router

---
+++ src/menu-app/menu-items.controller.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('MenuItemsController', MenuItemsController);
+
+MenuItemsController.$inject = ['$stateParams', 'MenuDataService'];
+function MenuItemsController($stateParams, MenuDataService) {
+    var $ctrl = this;
+    var category = $stateParams.category;
+    $ctrl.items = [];
+
+    $ctrl.$onInit = function () {
+        $ctrl.getItems();
+    }
+
+    $ctrl.getItems = function () {
+        MenuDataService.getItemsForCategory(category)
+            .then(function(result) {
+                $ctrl.items = result;
+            });
+    }
+
+    $ctrl.hasTwoSizes = function (index) {
+        var item = $ctrl.items[index];
+        if (item.large_portion_name && item.price_large
+            && item.small_portion_name && item.price_small) {
+                return true;
+        } else {
+                return false;
+        }
+    }
+
+}
+
+})();
--- src/routes.js
+++ src/routes.js
@@ -7,24 +7,33 @@ angular.module('MenuApp')
 RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 function RoutesConfig($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-  .state('home', {
-    url: '/',
-    templateUrl: 'src/templates/home.template.html'
-  })
+    $stateProvider
+    .state('home', {
+        url: '/',
+        templateUrl: 'src/templates/home.template.html'
+    })
 
-  .state("categories", {
-    url: '/categories',
-    templateUrl: 'src/templates/all-categories.template.html',
-    controller: 'AllCategoriesController as allCategories',
-    resolve: {
-        categories: ['MenuDataService', function (MenuDataService) {
-            return MenuDataService.getAllCategories();
-        }]
-    }
-  });
+    .state("categories", {
+        url: '/categories',
+        templateUrl: 'src/templates/all-categories.template.html',
+        controller: 'AllCategoriesController as allCategoriesCtrl',
+        resolve: {
+            categories: ['MenuDataService', function (MenuDataService) {
+                return MenuDataService.getAllCategories();
+            }]
+        }
+    })
 
-  $urlRouterProvider.otherwise('/');
+    .state("categories.menuItems", {
+        url: '/menu-items/{category}',
+        templateUrl: 'src/templates/all-items.template.html',
+        controller: 'MenuItemsController as itemsCtrl',
+        params: {
+            category: null
+        }
+    });
+
+    $urlRouterProvider.otherwise('/');
 
 }
 
